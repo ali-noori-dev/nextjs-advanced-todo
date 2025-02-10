96046f7c3cbc9309aff1a62410d3fb69a833a31b
ali-noori-dev <ali.noori.dev@gmail.com>
2025-02-11 02:47:08 +0330

    chore: initialize Next.js project with TypeScript, Prisma, and Sass

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { inter } from "@/app/ui/fonts";
+import "@styles/global.scss";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next.js Advanced Todo",
+  description: "A feature-rich todo app built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
