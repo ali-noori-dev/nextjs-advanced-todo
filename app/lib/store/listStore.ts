599a5ca44535ff472ca2ea39c90413ea47249c20
Ali Noori <ali.noori.dev@gmail.com>
2025-05-01 13:20:31 +0330

    refactor: extract list and card handling logic into separate functions for improved readability

--- app/lib/store/listStore.ts
+++ app/lib/store/listStore.ts
@@ -1,12 +1,9 @@
 "use client";
 
 import { createCard, createList } from "@/app/lib/api";
-import type { CardInput } from "@/app/lib/types";
-import type { Card, List } from "@prisma/client";
+import type { CardInput, ListWithCards } from "@/app/lib/types";
 import toast from "react-hot-toast";
-import { create } from "zustand";
-
-export type ListWithCards = List & { cards: Card[] };
+import { create, StateCreator } from "zustand";
 
 interface ListState {
   lists: ListWithCards[];
@@ -17,47 +14,53 @@ interface ListState {
   addCard: (listId: string, card: CardInput) => Promise<void>;
 }
 
+type SetState = Parameters<StateCreator<ListState>>[0];
+
+const handleAddList = async (title: string, set: SetState) => {
+  set({ loadingList: true });
+  try {
+    const newList = await createList(title);
+
+    set((state) => ({
+      lists: [...state.lists, { ...newList, cards: [] }],
+      loading: false,
+    }));
+  } catch (error) {
+    console.error("Failed to create list:", error);
+    toast.error("Failed to create list");
+    set({ loadingList: false });
+  }
+};
+
+const handleAddCard = async (
+  listId: string,
+  card: CardInput,
+  set: SetState
+) => {
+  set({ loadingCard: true });
+
+  try {
+    const newCard = await createCard(listId, card);
+
+    set((state) => ({
+      lists: state.lists.map((list) =>
+        list.id === listId ? { ...list, cards: [...list.cards, newCard] } : list
+      ),
+      loadingCard: false,
+    }));
+  } catch (error) {
+    console.error("Failed to add card:", error);
+    toast.error("Failed to add card");
+    set({ loadingCard: false });
+  }
+};
+
 export const useListStore = create<ListState>((set) => ({
   lists: [],
   loadingList: false,
   loadingCard: false,
 
   setLists: (lists) => set({ lists }),
-
-  addList: async (title) => {
-    set({ loadingList: true });
-    try {
-      const newList = await createList(title);
-
-      set((state) => ({
-        lists: [...state.lists, { ...newList, cards: [] }],
-        loading: false,
-      }));
-    } catch (error) {
-      console.error("Failed to create list:", error);
-      toast.error("Failed to create list");
-      set({ loadingList: false });
-    }
-  },
-
-  addCard: async (listId, card) => {
-    set({ loadingCard: true });
-
-    try {
-      const newCard = await createCard(listId, card);
-
-      set((state) => ({
-        lists: state.lists.map((list) =>
-          list.id === listId
-            ? { ...list, cards: [...list.cards, newCard] }
-            : list
-        ),
-        loading: false,
-      }));
-    } catch (error) {
-      console.error("Failed to add card:", error);
-      toast.error("Failed to add card");
-      set({ loadingCard: false });
-    }
-  },
+  addList: (title) => handleAddList(title, set),
+  addCard: (listId, card) => handleAddCard(listId, card, set),
 }));
