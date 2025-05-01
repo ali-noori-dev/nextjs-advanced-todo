79a8c3f97c7d70c4126a108c3b22089c9a1a7d5a
Ali Noori <ali.noori.dev@gmail.com>
2025-05-01 13:48:14 +0330

    refactor: enhance withCurrentUser function to support client and server modes

--- app/lib/middleware/auth.ts
+++ app/lib/middleware/auth.ts
@@ -1,13 +1,38 @@
+import { Routes } from "@/app/lib/constants";
 import { authOptions } from "@/auth";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export async function withCurrentUser(
-  handler: (email: string) => Promise<Response>
-) {
+type AuthMode = "client" | "server";
+
+async function withCurrentUser<T = Response>(
+  handler: (email: string) => Promise<T>,
+  options?: { mode?: AuthMode }
+): Promise<T | Response> {
   const session = await getServerSession(authOptions);
-  if (!session?.user?.email) {
-    return new Response("Unauthorized", { status: 401 });
+  const userEmail = session?.user?.email;
+
+  if (!userEmail) {
+    if (options?.mode === "client") {
+      return new Response("Unauthorized", { status: 401 });
+    } else {
+      redirect(Routes.Login);
+    }
   }
 
-  return handler(session.user.email);
+  return handler(userEmail);
+}
+
+export async function withCurrentUserAPI(
+  handler: (email: string) => Promise<Response>
+) {
+  return withCurrentUser(handler, { mode: "client" });
+}
+
+export async function withCurrentUserPage<T>(
+  handler: (email: string) => Promise<T>
+): Promise<T> {
+  const result = await withCurrentUser<T>(handler, { mode: "server" });
+
+  return result as T;
 }
