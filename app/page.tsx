0dba39940c8d3087b6e769c6bfbd710a1145b319
Ali Noori <ali.noori.dev@gmail.com>
2025-05-01 13:03:02 +0330

    feat: implement HomePage component with list retrieval and rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-export default function Home() {
-  return (
-    <div>
-      <main></main>
-      <footer></footer>
-    </div>
-  );
+import { getListsWithCards } from "@/app/lib/data";
+import { withCurrentUserPage } from "@/app/lib/middleware";
+import { Board } from "@/app/ui/board";
+
+export default async function HomePage() {
+  const lists = await withCurrentUserPage(getListsWithCards);
+
+  return <Board initialLists={lists} />;
 }
