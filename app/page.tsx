22e70edf89dc55cd3156f458e9a1aaf3eb63f6a1
Ali Noori <ali.noori.dev@gmail.com>
2025-05-03 14:03:13 +0330

    feat: add Header component to HomePage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { getListsWithCards } from "@/app/lib/data";
 import { withCurrentUserPage } from "@/app/lib/middleware";
 import { Board } from "@/app/ui/board";
+import { Header } from "@/app/ui/layout";
 
 export default async function HomePage() {
   const lists = await withCurrentUserPage(getListsWithCards);
 
-  return <Board initialLists={lists} />;
+  return (
+    <>
+      <Header />
+      <Board initialLists={lists} />
+    </>
+  );
 }
